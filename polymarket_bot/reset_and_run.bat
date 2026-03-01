a544ed9703243e93c05a11033659e3088d361e54
johnny <jcfor16@gmail.com>
2026-02-28 23:14:45 -0600

    Make slug-arg runs deterministic by disabling auto stepping/filter/url overrides

--- polymarket_bot/reset_and_run.bat
+++ polymarket_bot/reset_and_run.bat
@@ -9,7 +9,11 @@ set "SLUG_SUFFIX=%~1"
 if "%SLUG_SUFFIX%"=="" goto no_slug
 set "FORCE_MARKET_SLUG_CONTAINS=btc-updown-5m-%SLUG_SUFFIX%"
 set "FORCE_EVENT_SLUG=btc-updown-5m-%SLUG_SUFFIX%"
-echo Using runtime slug: btc-updown-5m-%SLUG_SUFFIX%
+set "USE_FILTER_ONLY=false"
+set "AUTO_SLUG_FROM_URL=false"
+set "AUTO_FORCE_SLUG_STEP=false"
+set "STEP_ON_MISS=false"
+echo Using runtime slug: btc-updown-5m-%SLUG_SUFFIX% (manual deterministic mode)
 :no_slug
 set "ENABLE_SLUG_PROMPT=false"
 
